fa77964abe964e672af5608ffc63bac40601fdb4
RastW <rast.leonhart@qq.com>
2022-08-28 16:59:21 +0800

    fizz_buzz

--- Rust/code/src/august/fizz_buzz.rs
+++ Rust/code/src/august/fizz_buzz.rs
@@ -1,10 +1,32 @@
 pub fn fizz_buzz(n: i32) -> Vec<String> {
-    let result = Vec::new();
-    (0..5)
-        .scan(0, |sum, v| {
-            *sum += v;
-            Some(*sum)
-        }).collect();
+    (1..n + 1)
+        .into_iter()
+        .map(|num| {
+            if num % 3 == 0 && num % 5 == 0 {
+                String::from("FizzBuzz")
+            } else if num % 3 == 0 {
+                String::from("Fizz")
+            } else if num % 5 == 0 {
+                String::from("Buzz")
+            } else {
+                num.to_string()
+            }
+        })
+        .collect()
+}
+
+pub fn fizz_buzz_one(n: i32) -> Vec<String> {
+    let mut result = Vec::<String>::new();
+    let mut i = 1;
+    while i <= n {
+        match i {
+            x if x % (3 * 5) == 0 => result.push("FizzBuzz".to_string()),
+            x if x % 3 == 0 => result.push("Fizz".to_string()),
+            x if x % 5 == 0 => result.push("Buzz".to_string()),
+            _ => result.push(i.to_string()),
+        }
+        i += 1;
+    }
     result
 }
 
@@ -12,6 +34,10 @@ pub fn fizz_buzz(n: i32) -> Vec<String> {
 #[cfg(test)]
 mod test {
 
+    use super::*;
+
     #[test]
-    fn test() {}
+    fn test() {
+        assert_eq!(vec!["1","2","Fizz"], fizz_buzz(3));
+    }
 }
