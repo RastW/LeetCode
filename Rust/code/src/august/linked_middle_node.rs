9f42d1c2aa95984df1eb553ae0db825d7f64c191
RastW <rast.leonhart@qq.com>
2022-09-07 17:55:26 +0800

    linked_middle_node

--- Rust/code/src/august/linked_middle_node.rs
+++ Rust/code/src/august/linked_middle_node.rs
@@ -15,6 +15,8 @@ impl ListNode {
     }
 }
 
+// 返回链表中间节点
+// 使用亏快慢指针，快指针走两步慢指针走一步，最终快指针走完的时候满指针走到中间节点
 pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let (mut slow, mut fast) = (&head, &head);
     while fast.as_ref().is_some() && fast.as_ref().unwrap().next.is_some() {
